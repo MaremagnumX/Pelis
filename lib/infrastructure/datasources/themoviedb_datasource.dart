d3806d757a96db6afab7ea071ced22bf146af14d
Kevin Villamizar <kvillamizar196@gmail.com>
2023-11-02 18:30:08 -0500

    Implementation of movie_mapper in moviedb_datasource

--- lib/infrastructure/datasources/themoviedb_datasource.dart
+++ lib/infrastructure/datasources/themoviedb_datasource.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:pelis/config/constants/environment.dart';
 import 'package:pelis/domain/datasources/movies_datasource.dart';
 import 'package:pelis/domain/entities/movie.dart';
+import 'package:pelis/infrastructure/mappers/movie_mapper.dart';
+import 'package:pelis/infrastructure/models/moviedb/moviedb_response.dart';
 
 class TheMovieDbDatasource extends MoviesDatasource {
   final dio = Dio(BaseOptions(
@@ -12,9 +14,13 @@ class TheMovieDbDatasource extends MoviesDatasource {
       }));
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
-    final reponse = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
-    
-    return [];
+    final response = await dio.get('/movie/now_playing');
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+    final List<Movie> movies = movieDBResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((moviedb) => MovieMapper.movieDBToEntitie(moviedb))
+        .toList();
+
+    return movies;
   }
 }
