72390070cb1100c62c2f0a940c90e73c7c36296e
Kevin Villamizar <kvillamizar196@gmail.com>
2023-11-05 18:38:59 -0500

    Movie slideshow implementation

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pelis/presentation/provider/movies/movies_providers.dart';
 import 'package:pelis/presentation/widgets/shared/custom_appbar.dart';
+import 'package:pelis/presentation/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -35,18 +36,7 @@ class _HomeViewState extends ConsumerState<_HomeView> {
     return Column(
       children: [
         const CustomAppbar(),
-        Expanded(
-          child: ListView.builder(
-            padding: const EdgeInsets.symmetric(vertical: 5),
-            itemCount: nowPlayingMovies.length,
-            itemBuilder: (context, index) {
-              final movie = nowPlayingMovies[index];
-              return ListTile(
-                title: Text(movie.title),
-              );
-            },
-          ),
-        )
+        MoviesSlideshow(movies: nowPlayingMovies),
       ],
     );
   }
