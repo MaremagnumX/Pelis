c3205f0eb6e3336474bfbc21077e7c22bf850913
Kevin Villamizar <kvillamizar196@gmail.com>
2023-11-15 16:59:12 -0500

    Movie screen appbar added

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -35,7 +35,62 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
       );
     }
     return Scaffold(
-      appBar: AppBar(title: Text('Movie id: ${widget.movieId}')),
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        slivers: [
+          _CustomSleaverAppbar(movie: movie),
+        ],
+      ),
     );
+  }
+}
+
+class _CustomSleaverAppbar extends StatelessWidget {
+  final Movie movie;
+  const _CustomSleaverAppbar({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SliverAppBar(
+        backgroundColor: Colors.black,
+        expandedHeight: size.height * 0.7,
+        foregroundColor: Colors.white,
+        flexibleSpace: FlexibleSpaceBar(
+          background: Stack(
+            children: [
+              SizedBox.expand(
+                child: Image.network(
+                  movie.posterPath,
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+              const SizedBox.expand(
+                child: DecoratedBox(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            stops: [0.7, 1],
+                            colors: [Colors.transparent, Colors.black87]))),
+              ),
+              const SizedBox.expand(
+                child: DecoratedBox(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            stops: [0.0, 0.3],
+                            colors: [Colors.black87, Colors.transparent]))),
+              )
+            ],
+          ),
+          titlePadding:
+              const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          title: Text(
+            movie.title,
+            style: const TextStyle(fontSize: 20),
+            textAlign: TextAlign.end,
+          ),
+        ));
   }
 }
