871c85df12d3e32227f34b391e3091c092664af7
Kevin Villamizar <kvillamizar196@gmail.com>
2023-11-16 23:22:59 -0500

    Display of the list of actors on the cinema screen 2

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,7 +1,9 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:pelis/domain/entities/movie.dart';
-import 'package:pelis/presentation/provider/movies/movie_info_provider.dart';
+import 'package:pelis/presentation/provider/providers.dart';
 
 class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'movie_screen';
@@ -20,6 +22,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
     super.initState();
 
     ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
+    ref.read(actorsByMovieProvider.notifier).loadActors(widget.movieId);
   }
 
   @override
@@ -106,9 +109,75 @@ class _MovieDetail extends StatelessWidget {
             ],
           ),
         ),
-        //TODO: lista actores de la pelicula
-        const SizedBox(height: 100),
+        _ActorByMovie(
+          movieId: movie.id.toString(),
+        ),
+        const SizedBox(height: 50),
       ],
+    );
+  }
+}
+
+class _ActorByMovie extends ConsumerWidget {
+  final String movieId;
+
+  const _ActorByMovie({required this.movieId});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final actorsByMovie = ref.watch(actorsByMovieProvider);
+    if (actorsByMovie[movieId] == null) {
+      return const CircularProgressIndicator(
+        strokeWidth: 2,
+      );
+    }
+
+    final actors = actorsByMovie[movieId]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+
+          return Container(
+            padding: const EdgeInsets.all(8),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FadeInRight(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      actor.profilePath,
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  actor.name,
+                  maxLines: 2,
+                ),
+                Text(
+                  actor.charcater ?? '',
+                  maxLines: 2,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      overflow: TextOverflow.ellipsis),
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: actors.length,
+      ),
     );
   }
 }
@@ -129,8 +198,14 @@ class _CustomSleaverAppbar extends StatelessWidget {
             children: [
               SizedBox.expand(
                 child: Image.network(
-                  movie.posterPath,
-                  fit: BoxFit.fitHeight,
+                  MediaQuery.of(context).orientation == Orientation.landscape
+                      ? movie.backdropPath
+                      : movie.posterPath,
+                  fit: BoxFit.cover,
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress != null) return const SizedBox();
+                    return FadeIn(child: child);
+                  },
                 ),
               ),
               const SizedBox.expand(
@@ -154,11 +229,11 @@ class _CustomSleaverAppbar extends StatelessWidget {
           ),
           titlePadding:
               const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          title: Text(
-            movie.title,
-            style: const TextStyle(fontSize: 20),
-            textAlign: TextAlign.end,
-          ),
+          // title: Text(
+          //   movie.title,
+          //   style: const TextStyle(fontSize: 20),
+          //   textAlign: TextAlign.end,
+          // ),
         ));
   }
 }
